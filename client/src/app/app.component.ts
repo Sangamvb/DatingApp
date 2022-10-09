acdaed542e5dc4b1e9e63f2e37dc82aad8c2395a
sangmeshwar <Sangamvb@users.noreply.github.com>
2022-10-09 22:53:03 +0530

    FirstCommit

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Dating Application';
+  users:any;
+
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.getUsers();
+  }
+  getUsers()
+  {
+    // this.http.get('https://localhost:5001/api/Users').subscribe(response=>{
+    //   this.users=response;
+    //     },error=>{
+    //       console.log(error);
+    //      })(')
+    //http://192.168.1.5:2000/api/Users
+    this.http.get('http://localhost:2001/api/Users').subscribe({
+      next:response=>this.users=response,
+      error:error=>console.log(error)
+    })
+  }
+}
