85bdf4d0b391d33c3b6c13fc9d6c1661bd6f9bad
sangmeshwar <Sangamvb@users.noreply.github.com>
2022-10-19 13:16:56 +0530

    Section 4 with extension
    Section 4 with extension

---
+++ API/Data/Migrations/20221009203050_userpasswordupdate.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class userpasswordupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordGash",
+                table: "Users",
+                newName: "PasswordHash");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "PasswordGash");
+        }
+    }
+}
